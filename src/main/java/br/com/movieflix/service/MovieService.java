f61cf5d89dc5bd1bdf0ee676f5470e33d63ced2b
juliolouzz <jlouz.dev@gmail.com>
2025-08-26 21:00:21 +0100

    fix: fix response payload of Post Movie route to save new Movie, was returning null on movie categories

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,10 +1,13 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,13 +15,31 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreaming(this.findStreamings(movie.getStreaming()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId())
+                .ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId())
+                .ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 
 }
