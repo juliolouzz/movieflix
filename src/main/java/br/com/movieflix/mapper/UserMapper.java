cd5b73b041cd4d30021c9c8bc7e2e5d38a8c0636
juliolouzz <jlouz.dev@gmail.com>
2025-09-01 21:58:11 +0100

    fix: adjust toUser method to use UserRequest on UserMapper

--- src/main/java/br/com/movieflix/mapper/UserMapper.java
+++ src/main/java/br/com/movieflix/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package br.com.movieflix.mapper;
 
+import br.com.movieflix.controller.request.UserRequest;
 import br.com.movieflix.controller.response.UserResponse;
 import br.com.movieflix.entity.User;
 import lombok.experimental.UtilityClass;
@@ -7,7 +8,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class UserMapper {
 
-    public static User toUser(UserResponse request) {
+    public static User toUser(UserRequest request) {
         return User.builder()
                 .name(request.name())
                 .email(request.email())
