72c1c2cd5c1ef4b8907048c04e9a37efc1e39760
juliolouzz <jlouz.dev@gmail.com>
2025-09-04 14:21:08 +0100

    feat: Implement Spring Security, make Class SecurityConfig

--- src/main/java/br/com/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/controller/AuthController.java
@@ -1,5 +1,6 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.LoginRequest;
 import br.com.movieflix.controller.request.UserRequest;
 import br.com.movieflix.controller.response.UserResponse;
 import br.com.movieflix.entity.User;
@@ -8,6 +9,9 @@ import br.com.movieflix.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest request){
         User savedUser = userService.save(UserMapper.toUser(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+        UsernamePasswordAuthenticationToken userAndPass =
+                new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+
 
     }
 }
